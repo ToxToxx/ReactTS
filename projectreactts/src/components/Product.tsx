fbca3f9ea542ce0e1f06d9d68ceb5042be2b40c4
Matvey <sinema3d00@gmail.com>
2024-09-09 17:41:54 +0300

    some store data work

--- projectreactts/src/components/Product.tsx
+++ projectreactts/src/components/Product.tsx
@@ -1,8 +1,17 @@
-export function Product(props: any){
-    console.log(props)
+import { IProduct } from "../models"
+
+interface ProductProps{
+    product: IProduct
+}
+
+export function Product({product}: ProductProps){
     return(
         <div
-        className="border py-2 px-4 rounded flex flex-col items-center mb-2">
-             Product </div>
+        className="border py-2 px-4 rounded flex flex-col items-center mb-2"
+        >
+        <img src={product.image} className="w-1/6" alt={product.title}/>
+        <p> {product.title} </p>
+        <span className="font-bold">{product.price}</span>
+        </div>   
     )
 }
