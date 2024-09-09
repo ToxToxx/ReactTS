cffaa586dd2192b920cfacb7dcdfa2206174eaef
Matvey <sinema3d00@gmail.com>
2024-09-09 17:17:25 +0300

    export function

--- projectreactts/src/App.tsx
+++ projectreactts/src/App.tsx
@@ -1,19 +1,14 @@
 import React, {useState, createElement as e} from 'react';
-import logo from './logo.svg';
+import {Product} from './components/Product'
 
 
 
 function App() {
-  const [count, setCount] = useState(0);
-  
-  return e('div', {className: 'container'}, [
-    e( 'h1',  {className: 'font-bold', key: 1}, `Counter: ${count}`),
-    e( 'button',  {
-      className: 'py-2 px-4 border',
-      key: 2,
-      onClick: () => setCount(count + 1)
-    }, 'Click me')
-  ])
+  return (
+    <div className ="container mx-auto max-w-2xl pt-5">
+      <Product />
+    </div>
+  )
 }
 
 export default App;
