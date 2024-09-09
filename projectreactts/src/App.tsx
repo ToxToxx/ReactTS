f6292d68f6747f35a546097474d280d0269aa822
Matvey <sinema3d00@gmail.com>
2024-09-09 17:12:29 +0300

    example of jsx code

--- projectreactts/src/App.tsx
+++ projectreactts/src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState, createElement as e} from 'react';
 import logo from './logo.svg';
 
+
+
 function App() {
-  return(
-    <h1> Hello </h1>
-  )
+  const [count, setCount] = useState(0);
+  
+  return e('div', {className: 'container'}, [
+    e( 'h1',  {className: 'font-bold', key: 1}, `Counter: ${count}`),
+    e( 'button',  {
+      className: 'py-2 px-4 border',
+      key: 2,
+      onClick: () => setCount(count + 1)
+    }, 'Click me')
+  ])
 }
 
 export default App;
