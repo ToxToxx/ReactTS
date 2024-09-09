b3c514276604eafcf805d04a4bb502580aa79ccc
Matvey <sinema3d00@gmail.com>
2024-09-09 17:29:07 +0300

    made interface for models

--- projectreactts/src/App.tsx
+++ projectreactts/src/App.tsx
@@ -1,12 +1,12 @@
 import React, {useState, createElement as e} from 'react';
 import {Product} from './components/Product'
-
+import {products} from './data/products'
 
 
 function App() {
   return (
     <div className ="container mx-auto max-w-2xl pt-5">
-      <Product />
+      <Product product={products[0]} />
     </div>
   )
 }
--- projectreactts/src/components/Product.tsx
+++ projectreactts/src/components/Product.tsx
@@ -1,5 +1,8 @@
-export function Product(){
+export function Product(props: any){
+    console.log(props)
     return(
-        <div> Product </div>
+        <div
+        className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+             Product </div>
     )
 }
