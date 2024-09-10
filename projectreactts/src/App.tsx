39b637e3d852ca5a01040a9a9d7ce0ba5f4cc115
Matvey <sinema3d00@gmail.com>
2024-09-10 19:56:02 +0300

    get from api products

--- projectreactts/src/App.tsx
+++ projectreactts/src/App.tsx
@@ -1,13 +1,27 @@
-import React, {useState, createElement as e} from 'react';
+import React, {useState, createElement as e, useEffect} from 'react';
 import {Product} from './components/Product'
 import {products} from './data/products'
+import axios from 'axios';
+import { IProduct } from './models';
 
 
 function App() {
+
+  async function fetchProducts(){
+    const response = 
+    await axios.get<IProduct[]>
+    ('https://fakestoreapi.com/products?limit=7')
+    console.log(response)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
   return (
     <div className ="container mx-auto max-w-2xl pt-5">
-      <Product product={products[0]} />
-      <Product product={products[1]} />
+
+      { products.map(product => <Product product={product} key={product.id} />)}
     </div>
   )
 }
--- projectreactts/src/components/Product.tsx
+++ projectreactts/src/components/Product.tsx
@@ -8,6 +8,10 @@ interface ProductProps{
 export function Product({product}: ProductProps){
     const [details, setDetails] = useState(false)
 
+    const btnBgClassName = details ? 'bg-yellow-400' : 'bg-red-400'
+
+    const btnClasses = ['py-2 px-4 border' , btnBgClassName]
+
     return(
         <div
         className="border py-2 px-4 rounded flex flex-col items-center mb-2"
@@ -16,13 +20,14 @@ export function Product({product}: ProductProps){
         <p> {product.title} </p>
         <span className="font-bold">{product.price}</span>
         <button 
-            className="py-2 px-4 border bg-yellow-400" 
+            className={btnClasses.join(' ')}
             onClick = {() => setDetails(prev => !prev)}> 
                 {details ? 'Hide details' : 'Show details'}
         </button>
 
         { details && <div>
             <p> {product.description}   </p>
+            <p>Rate: <span style={{ fontWeight: 'bold' }}>{product.rating.rate}</span></p>
         </div>}
         
         </div>   
