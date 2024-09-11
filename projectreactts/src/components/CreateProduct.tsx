60b3dbb12994d8f479df321383f7560cd004a51e
Matvey <sinema3d00@gmail.com>
2024-09-11 17:19:37 +0300

    add error handling and making creating product

--- projectreactts/src/components/CreateProduct.tsx
+++ projectreactts/src/components/CreateProduct.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { IProduct } from "../models";
+import axios from "axios";
+import { ErrorMessage } from "./ErrorMessage";
+
+
+const productData: IProduct = {
+    title: '',
+    price: 13.5,
+    description: 'lorem ipsum set',
+    image: 'https://i.pravatar.cc',
+    category: 'electronic',
+    rating: {
+        rate: 42,
+        count: 10
+    }
+} 
 
 export function CreateProduct(){
+    const [value, setValue] = useState('')
+    const [error, setError] = useState('')
     
-    const submitHandler = (event: React.FormEvent) => {
+    const submitHandler = async (event: React.FormEvent) => {
         event.preventDefault()
+        
+        if(value.trim().length==0){
+            setError('Please enter valid title.')
+            return
+        }
+
+        productData.title = value
+
+        const response = await axios.post<IProduct>('https://fakestoreapi.com/products', productData)
+    }
+
+    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value)
     }
 
     return(
@@ -12,8 +43,11 @@ export function CreateProduct(){
             type='text'
             className="border py-2 px-4 mb-2 w-full outline-0"
             placeholder="Enter product title..."
+            value = {value}
+            onChange={changeHandler}
             />
 
+            {error && <ErrorMessage error = {error} />}
             <button type='submit' className="py-2 px-4 border bg-yellow-400 hover:text-white">
                 Create
             </button>
