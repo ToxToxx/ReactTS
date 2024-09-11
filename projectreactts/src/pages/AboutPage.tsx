1a2dda5fdbbb06c3ac5b8909d1d38fefb979b972
Matvey <sinema3d00@gmail.com>
2024-09-11 20:33:41 +0300

    made a mocking about page

---
+++ projectreactts/src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export function AboutPage(){
+    return(
+        <div> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat facilis officia repellat optio quod sint, autem voluptatibus, itaque, dolor magnam quisquam explicabo. Laboriosam doloremque non inventore amet deleniti obcaecati unde quia soluta tempore? Quas autem ea cupiditate illo ut dignissimos eius eveniet corporis assumenda laboriosam, alias, sit quae odio eum?</div>
+    )
+}
